be96c5697ee5d3599a0365d3a2a6934e339b2bff
snim2 <mount.sarah@gmail.com>
2015-06-15 10:45:37 +0100

    Test mixed and single width instructions using the different run() methods

--- pydgin/tests/test_mixed_width.py
+++ pydgin/tests/test_mixed_width.py
@@ -71,7 +71,32 @@ class ExampleInstruction(object):
         self.str  = str
 
 
-class ExampleMachine(Sim):
+class SingleWidthMachine(Sim):
+
+    def __init__(self):
+        Sim.__init__(self, "Example", inst_sizes=[16], jit_enabled=True) # Breaks if False.
+
+    def decode(self, bits):
+        inst_str, exec_fun = decode(bits)
+        return ExampleInstruction(bits, inst_str), exec_fun
+
+
+    def load_program(self, instructions, **args):
+        """Load the program into a memory object.
+        Instructions should take the form of a list of tuples containing an
+        instruction and its width. e.g.:
+            [ (0b0, 32) ]
+        """
+        mem = Memory(size=2**18, byte_storage=True)
+        written_so_far = 0
+        for data, width in instructions:
+            num_bytes = width / 8
+            mem.write(written_so_far, num_bytes, data)
+            written_so_far += num_bytes
+        self.state = ExampleState(mem, Debug(flags=['insts', 'mem', 'rf', 'regdump']))
+
+
+class MixedWidthMachine(Sim):
 
     def __init__(self):
         Sim.__init__(self, "Example", inst_sizes=[32, 16], jit_enabled=True) # Breaks if False.
@@ -95,7 +120,8 @@ class ExampleMachine(Sim):
             written_so_far += num_bytes
         self.state = ExampleState(mem, Debug(flags=['insts', 'mem', 'rf', 'regdump']))
 
-init_sim(ExampleMachine())
+init_sim(SingleWidthMachine())
+init_sim(MixedWidthMachine())
 
 
 def test_only16bit_instructions():
@@ -104,7 +130,7 @@ def test_only16bit_instructions():
     instructions = [(0b0000000110100010, 16),                  # nop16
                     (0b0000000111000010, 16),                  # halt16
                     ]
-    machine = ExampleMachine()
+    machine = SingleWidthMachine()
     machine.load_program(instructions)
     machine.run()
     # nop16 increments by 2, halt16 increments by 2.
@@ -116,7 +142,7 @@ def test_mixed_width_instructions():
                     (0b11111111111111111111111111111111, 32),  # hello32
                     (0b0000000111000010, 16),                  # halt16
                     ]
-    machine2 = ExampleMachine()
+    machine2 = MixedWidthMachine()
     machine2.load_program(instructions)
     machine2.run()
     # nop16 increments by 2, hello32 increments by 4, halt16 increments by 2.
